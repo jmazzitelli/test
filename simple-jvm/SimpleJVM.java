dfeac0127b27dff72a23ac04d7f7681e83cde9de
John Mazzitelli <mazz@redhat.com>
2017-03-10 00:24:21 -0500

    to test javaagent with mbean

---
+++ simple-jvm/SimpleJVM.java
@@ -0,0 +1,79 @@
+import javax.management.*;
+import java.lang.management.*;
+
+public class SimpleJVM implements SimpleMXBean {
+    public static final void main(String[] args) {
+        try {
+            MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+            mbs.registerMBean(new SimpleJVM(), new ObjectName("org.hawkular.test:type=simple"));
+        } catch (Exception e) {
+            System.out.println("Failed to register the MBean");
+            e.printStackTrace();
+        }
+
+        synchronized (SimpleJVM.class) {
+            System.out.println("Going in a coma...");
+            try {
+                SimpleJVM.class.wait();
+            } catch (Exception e) {
+            }
+            System.out.println("Woke up from coma and exiting.");
+        }
+    }
+
+   // JMX Attributes
+
+    public String getTestString() { return "Test string from SimpleJVM MBean"; }
+
+    public int     getTestIntegerPrimitive() { return 12345; }
+    public Integer getTestInteger()          { return 54321; }
+
+    public boolean getTestBooleanPrimitive() { return false; }
+    public Boolean getTestBoolean()          { return true; }
+
+    public long getTestLongPrimitive()       { return 123456789L; }
+    public Long getTestLong()                { return 987654321L; }
+
+    public double getTestDoublePrimitive()   { return (double) 1.23456789; }
+    public Double getTestDouble()            { return (double) 9.87654321; }
+
+    public float getTestFloatPrimitive()     { return (float) 3.14; }
+    public Float getTestFloat()              { return (float) 6.28; }
+
+    public short getTestShortPrimitive()     { return (short) 12; }
+    public Short getTestShort()              { return (short) 21; }
+
+    public char      getTestCharPrimitive()  { return (char) 'a'; }
+    public Character getTestChar()           { return (char) 'z'; }
+
+    public byte getTestBytePrimitive()       { return (byte) 1; }
+    public Byte getTestByte()                { return (byte) 2; }
+
+    // JMX Operations
+
+    public String testOperationPrimitive(String s, int i, boolean b, long l, double d, float f, short h, char c, byte y) {
+        return String.format("string=%s, int=%s, boolean=%s, long=%s, double=%s, float=%s, short=%s, char=%s, byte=%s",
+                             s,
+                             String.valueOf(i),
+                             String.valueOf(b),
+                             String.valueOf(l),
+                             String.valueOf(d),
+                             String.valueOf(f),
+                             String.valueOf(h),
+                             String.valueOf(c),
+                             String.valueOf(y));
+    }
+
+    public String testOperation(String s, Integer i, Boolean b, Long l, Double d, Float f, Short h, Character c, Byte y) {
+        return String.format("String=%s, Int=%s, Boolean=%s, Long=%s, Double=%s, Float=%s, Short=%s, Char=%s, Byte=%s",
+                             s,
+                             (i == null) ? "null" : i.toString(),
+                             (b == null) ? "null" : b.toString(),
+                             (l == null) ? "null" : l.toString(),
+                             (d == null) ? "null" : d.toString(),
+                             (f == null) ? "null" : f.toString(),
+                             (h == null) ? "null" : h.toString(),
+                             (c == null) ? "null" : c.toString(),
+                             (y == null) ? "null" : y.toString());
+    }
+}
---
+++ simple-jvm/SimpleMXBean.java
@@ -0,0 +1,39 @@
+import javax.management.*;
+
+@MXBean
+public interface SimpleMXBean {
+
+    // JMX Attributes
+
+    String getTestString();
+
+    int     getTestIntegerPrimitive();
+    Integer getTestInteger();
+
+    boolean getTestBooleanPrimitive();
+    Boolean getTestBoolean();
+
+    long getTestLongPrimitive();
+    Long getTestLong();
+
+    double getTestDoublePrimitive();
+    Double getTestDouble();
+
+    float getTestFloatPrimitive();
+    Float getTestFloat();
+
+    short getTestShortPrimitive();
+    Short getTestShort();
+
+    char      getTestCharPrimitive();
+    Character getTestChar();
+
+    byte getTestBytePrimitive();
+    Byte getTestByte();
+
+    // JMX Operations
+
+    String testOperationPrimitive(String s, int i, boolean b, long l, double d, float f, short h, char c, byte y);
+    String testOperation(String s, Integer i, Boolean b, Long l, Double d, Float f, Short h, Character c, Byte y);
+
+}
