9494cd8fd154c92319904232f6d7ceeb8b8b448d
John Mazzitelli <mazz@redhat.com>
2021-04-19 11:35:27 -0400

    poc of perf enhancement when adding labels to namespaces

--- perf-ansible/get.sh
+++ perf-ansible/get.sh
@@ -1,5 +1,8 @@
 NUM_NAMESPACES=${1:-2}
 for i in $(seq ${NUM_NAMESPACES}); do
   echo "==NAMESPACE [test${i}]"
+  echo "  Labels=$(kubectl get ns test${i} -o jsonpath='{.metadata.labels}')"
+  echo "  Annotations=$(kubectl get ns test${i} -o jsonpath='{.metadata.annotations}')"
+  echo "  Resources:"
   kubectl get sa -n test${i} -l app=kiali -o name
 done
